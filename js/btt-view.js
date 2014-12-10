fc329ed98c71831bca7ce5b6707df25199fbdbf3
Kyle Smith <akam9865@gmail.com>
2014-12-09 17:12:40 -0800

    win small board, mark big board

--- js/btt-view.js
+++ js/btt-view.js
@@ -5,15 +5,71 @@
 	
 	var View = BTT.View = function ($boardEl) {
 		this.$boardEl = $boardEl;
-		
+		this.game = makeBoardGrid();
+
 		this.setupBoards();
+		this.bindEvents();
 	};
+	
+	function makeBoardGrid() {
+		var grid = [];
+		
+		for (var i = 0; i < 3; i++) {
+			grid.push([]);
+			for (var j = 0; j < 3; j++) {
+
+				grid[i].push(new BTT.Game());
+			}
+		}
+		return grid;
+	};
+	
+	View.prototype.bindEvents = function () {
+		var view = this;
+		this.$boardEl.on("click", "div.outer-cell", function (event){
+			var $littleSquare = $(event.target);
+			var $bigSquare = $(event.currentTarget);
+
+			view.makeMove($littleSquare, $bigSquare);
+			
+		}).bind(this);
+	};
+	
+  View.prototype.makeMove = function ($littleSquare, $bigSquare) {
+		var innerPos = $littleSquare.data("inner-pos");
+		var outerPos = $bigSquare.data("outer-pos");
+		var game = this.game[outerPos[0]][outerPos[1]];
+
+		var currentPlayer = game.currentPlayer;
+		
+		if (game.board.isEmptyPos(innerPos)) {
+			game.playMove(innerPos);
+			$littleSquare.addClass(currentPlayer);
+			$littleSquare.text(currentPlayer);
+
+		} else {
+			alert("square is already occupied");
+		}
+		
+		if (game.isOver()) {
+			// this.$boardEl.off("click");
+			// turn off only this specific board
+			
+			var winner = game.winner();
+			$bigSquare.text(winner);
+			
+			// if (winner) {
+			// 	$(".win-msg").append(winner + " wins");
+			// } else {
+			// 	$(".win-msg").append("draw");
+			// }
+		}
+  };
+	
 	
 	View.prototype.setupBoards = function () {
 		this.setupBigBoard();
-
 		var $bigBoard = $(".outer-cell");
-
 
 		for (var k = 0; k < 9; k++) {
 			for (var i = 0; i < 3; i++) {			
@@ -28,14 +84,10 @@
 				$($bigBoard[k]).append($row);
 			}
 		}
-		
-		
 	};
 	
 
 	View.prototype.setupBigBoard = function (){
-
-		
 		for (var i = 0; i < 3; i++) {
 			var $row = $('<div class="outer-row"></div>');
 
